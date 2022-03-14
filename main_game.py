09c9964fa0ef3051ff9ebed52467fd4d4a9ddeb4
Bismaon <64341829+Bismaon@users.noreply.github.com>
2022-03-14 20:03:12 +0700

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -1,5 +1,21 @@
+from distutils.util import change_root
 import tkinter as Tk
 
 class GameOfLife:
     def __init__(self):
         self.window=Tk.tk()
+        
+    def creation_de_gride(self):
+        for row in range():
+            for column in range():
+
+"""    def neighbor(self, position?):
+        find position, neighbors, add it to a global list
+
+    def vivant(self):
+        if trois neighbor or deux neighbors:
+            vivant, reste non change
+        elif plus de trois neighbors or moins de deux neighbors:
+            morte"""
+            celulle --> widgets --> liste --> matrice --> celulle --> vivante (color noire)/morte(color blanche)
+
